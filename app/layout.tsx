506102e6c5b088e816f0727902399281cefeaded
Dagim <dagimgetaW27@gmail.com>
2025-08-27 01:19:10 +0300

    intial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-"use client";
 import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "./provide";
@@ -69,19 +68,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="canonical" href="https://dagim.dev" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-      </head>
-
+    <html lang="en">
       <body className={`${poppins.variable} ${sacramento.variable}`}>
         <ThemeProvider
           attribute="class"
