76398835b0f49c7fff21c6a26efafa6518fe03ed
Dagim <dagimgetaW27@gmail.com>
2025-08-27 01:22:56 +0300

    intial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -68,7 +68,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="canonical" href="https://dagim.dev" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+      </head>
+
       <body className={`${poppins.variable} ${sacramento.variable}`}>
         <ThemeProvider
           attribute="class"
