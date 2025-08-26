270ef39ff8ea12aa19c3a615913412dce5d4e8d1
Dagim <dagimgetaW27@gmail.com>
2025-08-27 01:06:48 +0300

    intial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
+"use client";
 import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "./provide";
-import Head from "next/head";
 import { ThemeClientWrapper } from "./ThemeClientWrapper";
-
 import { Poppins, Sacramento } from "next/font/google";
 
 const poppins = Poppins({
@@ -45,7 +44,7 @@ export const metadata: Metadata = {
     siteName: "Dagim.dev",
     images: [
       {
-        url: "/og-image.png",
+        url: "https://dagim.dev/og-image.png",
         width: 1200,
         height: 630,
         alt: "Dagim Portfolio Preview",
@@ -58,25 +57,23 @@ export const metadata: Metadata = {
     card: "summary_large_image",
     title: "Dagim's Portfolio",
     description:
-      "Dagim Getaw is a passionate Ethiopian software engineer with expertise in modern web development, machine learning, AI, and full-stack technologies..",
-    images: ["/og-image.png"],
+      "Dagim Getaw is a passionate Ethiopian software engineer with expertise in modern web development, machine learning, AI, and full-stack technologies.",
+    images: ["https://dagim.dev/og-image.png"],
     creator: "@dagimdev",
   },
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta
-          name="description"
-          content="Dagim Getaw is a passionate Ethiopian software engineer with expertise in modern web development, machine learning, AI, and full-stack technologies."
-        />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="canonical" href="https://dagim.dev" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
